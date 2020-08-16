c0690e478a66a812ad15ac1b4e99e116e87e5ae1
davidsmiles <ugberodavid@gmail.com>
2020-08-16 14:27:01 +0100

    making transactions

--- app.py
+++ app.py
@@ -7,23 +7,28 @@ load_dotenv('.env')
 infura_url = os.environ.get('INFURA_URL')
 ganache_url = "http://127.0.0.1:7545"
 
-web3 = Web3(Web3.HTTPProvider(infura_url))
+web3 = Web3(Web3.HTTPProvider(ganache_url))
 
 
-# latest block number
+# # latest block number
 print(web3.eth.blockNumber)
 
+# transaction
+receiver = '0xEa4E792A1ac8C55c4d0171dB28Bda0c5E92586dD'
+sender = '0xc2d78Fa063B3357964442b3fc3E6F095501B6150'
+sender_pk = 'd5f02c4f726619da2c32bef51e88c1eb4652b24a7d39930eb69ffcf3549d8c03'
 
-# checking account balance
-account = web3.toChecksumAddress("0xe9f7ecae3a53d2a67105292894676b00d1fab785")
-balance = web3.eth.getBalance(account)
-print(web3.fromWei(balance, "Ether"))
+nonce = web3.eth.getTransactionCount(sender)
+tx = {
+    'nonce': nonce,
+    'to': receiver,
+    'value': web3.toWei('2', 'ether'),
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei')
+}
 
-
-# simple contracts
-abi = json.loads('[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]')
-address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'
-
-contract = web3.eth.contract(address=address, abi=abi)
-balance = contract.functions.balanceOf(account).call()
-print(web3.fromWei(balance, "Ether"))
+# sign account
+signed_tx = web3.eth.account.signTransaction(tx, sender_pk)
+# get hash
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+print(web3.toHex(tx_hash))
