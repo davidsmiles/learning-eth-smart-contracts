07e879b6a206fe35688485ecb209810779bab0f9
davidsmiles <ugberodavid@gmail.com>
2020-08-19 01:27:08 +0100

    code refactor

--- transaction.py
+++ transaction.py
@@ -10,13 +10,10 @@ ganache_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
 
-# # latest block number
-print(web3.eth.blockNumber)
-
 # transaction
-receiver = '0xEa4E792A1ac8C55c4d0171dB28Bda0c5E92586dD'
-sender = '0xc2d78Fa063B3357964442b3fc3E6F095501B6150'
-sender_pk = 'd5f02c4f726619da2c32bef51e88c1eb4652b24a7d39930eb69ffcf3549d8c03'
+receiver = '0x60738cAD8b84814BDbE9940733B2F2325eAab215'
+sender = '0x75390aC058980de51c261D6040BF840A2EB24375'
+sender_pk = '2ed13170c727e8cc080e2eccdc3dfeba276715b96030307bf6108a26ac4f8884'
 
 nonce = web3.eth.getTransactionCount(sender)
 tx = {
@@ -27,11 +24,10 @@ tx = {
     'gasPrice': web3.toWei('50', 'gwei')
 }
 
-# sign account
+# sign account if default account is not set
 signed_tx = web3.eth.account.signTransaction(tx, sender_pk)
-# get hash
-tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-print(web3.toHex(tx_hash))
+web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
-# read transaction
-print(web3.eth.getTransaction(tx_hash))
+# if default account is set
+web3.eth.defaultAccount = web3.eth.accounts[0]
+web3.eth.sendTransaction(tx)
